1ea13b8b0998b829385550af58861301845bcab9
Dev07 <dev07@192.168.0.103>
2018-12-02 20:24:49 +0200

    test list todos

--- server/server.js
+++ server/server.js
@@ -15,12 +15,27 @@ app.post('/todos', (req, res) => {
   });
 
   todo.save().then((doc) => {
-    res.send(doc);
+    res.status(200).send(doc);
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+
+    res.send({'todos':todos});
+  }).catch((e)=>{
+    res.status(400).send({'error':e});
+  });
+})
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+
+module.exports = {
+  app
+};
