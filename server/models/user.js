7361c7413e0ad7a7ad7d8e69e6a5e4393355e2f2
Dev07 <dev07@dev07s-mbp.zte.com.cn>
2018-12-17 19:56:41 +0200

    testing

--- server/models/user.js
+++ server/models/user.js
@@ -51,7 +51,6 @@ UserSchema.methods.generateAuthToken = function()
 
   return user.save().then(()=>{
 
-  
     return token;
   });
 
@@ -79,6 +78,33 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+
+UserSchema.statics.findByCredentials = function(email,password){
+  User = this;
+
+ return User.findOne({email}).then((user)=>{
+
+  if(!user)
+  return Promise.reject({'error':'user not found'});
+
+  return new Promise((resolve, reject) => {
+    // Use bcrypt.compare to compare password and user.password
+    bcrypt.compare(password, user.password, (err, res) => {
+      if (res) {
+        resolve(user);
+      } else {
+        reject();
+      }
+    });
+  });
+
+ });
+  
+  
+
+
+}
 
 UserSchema.pre('save',function (next){
 
